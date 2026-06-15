b1f35a8a995e8affe64d71ab84dd9b20e9c1eece
s. galiamov <6alyamov@gmail.com>
2026-06-15 14:42:41 +0200

    QUOT_SWAP

--- keyboards/0_custom/keymaps/default/keymap.c
+++ keyboards/0_custom/keymaps/default/keymap.c
@@ -28,6 +28,7 @@ enum custom_keycodes {
     TEMP_EN = QK_USER,
     ARROW_FAT,  // =>
     ARROW_THIN, // ->
+    QUOT_SWAP,  // _BASE: " when unshifted, ' when shifted
 };
 
 // Pressing both Shift keys simultaneously (held) toggles _BASE <-> _QWERTY.
@@ -51,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_ESC,      KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       XXXXXXX,     XXXXXXX,             XXXXXXX,     XXXXXXX,     KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      KC_PGUP,
         KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_F6,       XXXXXXX,             XXXXXXX,     KC_F7,       KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_PGDN,
-        KC_SCLN,     KC_J,        KC_M,        KC_U,        KC_B,        KC_Q,        KC_PAUS,     XXXXXXX,             XXXXXXX,     KC_PSCR,     KC_Z,        KC_H,        KC_T,        KC_C,        KC_MINS,     KC_EQL,
-        KC_TAB,      KC_L,        KC_R,        KC_E,        KC_O,        KC_P,        KC_SLSH,     KC_BSLS,             KC_INS,      KC_QUOT,     KC_K,        KC_I,        KC_N,        KC_A,        KC_S,        KC_ENT,
+        KC_SCLN,     KC_J,        KC_M,        KC_U,        KC_B,        KC_Q,        KC_PSCR,     XXXXXXX,             XXXXXXX,     KC_PAUS,     KC_Z,        KC_H,        KC_T,        KC_C,        KC_MINS,     KC_EQL,
+        KC_TAB,      KC_L,        KC_R,        KC_E,        KC_O,        KC_P,        KC_SLSH,     KC_BSLS,             KC_INS,      QUOT_SWAP,   KC_K,        KC_I,        KC_N,        KC_A,        KC_S,        KC_ENT,
         KC_DEL,      KC_W,        KC_F,        KC_Y,        KC_D,        KC_X,        KC_HOME,     KC_END,              KC_CAPS,     KC_COMM,     KC_DOT,      KC_G,        KC_V,        KC_LBRC,     KC_RBRC,     KC_BSPC,
         KC_LALT,     MO(_FN),     KC_APP,      KC_LCMD,     KC_LCTL,     KC_SPC,      KC_LSFT,     KC_ENT,              TD(TD_LALT), TD(TD_RSFT), KC_SPC,      TD(TD_RCTL), KC_LEFT,     KC_UP,       KC_DOWN,     KC_RGHT
     ),
@@ -60,16 +61,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT(
         _______,     _______,     _______,     _______,     _______,     _______,     XXXXXXX,     XXXXXXX,             XXXXXXX,     XXXXXXX,     _______,     _______,     _______,     _______,     _______,     _______,
         _______,     KC_P1,       KC_P2,       KC_P3,       KC_P4,       KC_P5,       _______,     XXXXXXX,             XXXXXXX,     _______,     KC_P6,       KC_P7,       KC_P8,       KC_P9,       KC_P0,       _______,
-        _______,     KC_MPRV,     KC_MNXT,     XXXXXXX,     KC_VOLD,     KC_VOLU,     KC_MPLY,     XXXXXXX,             XXXXXXX,     KC_NUM,      XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     KC_PMNS,     KC_PPLS,
+        _______,     A(KC_V),     C(KC_W),     C(KC_Y),     LCS(KC_B),   XXXXXXX,     KC_NUM,      XXXXXXX,             XXXXXXX,     KC_MPLY,     KC_MPRV,     KC_MNXT,     KC_VOLD,     KC_VOLU,     KC_PMNS,     KC_PPLS,
         _______,     C(KC_A),     C(KC_S),     C(KC_H),     C(KC_F),     XXXXXXX,     KC_PSLS,     _______,             _______,     _______,     XXXXXXX,     ARROW_FAT,   ARROW_THIN,  XXXXXXX,     KC_PAST,     KC_PEQL,
-        _______,     C(KC_Z),     C(KC_X),     C(KC_C),     C(KC_V),     XXXXXXX,     _______,     _______,             KC_LSCR,     KC_PCMM,     KC_PDOT,     XXXXXXX,     XXXXXXX,     _______,     _______,     _______,
+        _______,     C(KC_Z),     C(KC_X),     C(KC_C),     C(KC_V),     C(KC_B),     _______,     _______,             KC_LSCR,     KC_PCMM,     KC_PDOT,     XXXXXXX,     XXXXXXX,     _______,     _______,     _______,
         _______,     TO(_BASE),   _______,     _______,     _______,     _______,     _______,     KC_PENT,             _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______
     ),
 
     [_QWERTY] = LAYOUT(
         KC_ESC,      KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       XXXXXXX,     XXXXXXX,             XXXXXXX,     XXXXXXX,     KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      KC_PGUP,
         KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_F6,       XXXXXXX,             XXXXXXX,     KC_F7,       KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_PGDN,
-        KC_SCLN,     KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_PAUS,     XXXXXXX,             XXXXXXX,     KC_PSCR,     KC_Y,        KC_U,        KC_I,        KC_O,        KC_MINS,     KC_EQL,
+        KC_SCLN,     KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_PSCR,     XXXXXXX,             XXXXXXX,     KC_PAUS,     KC_Y,        KC_U,        KC_I,        KC_O,        KC_MINS,     KC_EQL,
         KC_TAB,      KC_A,        KC_S,        KC_D,        KC_F,        KC_G,        KC_SLSH,     KC_BSLS,             KC_INS,      KC_QUOT,     KC_H,        KC_J,        KC_K,        KC_L,        KC_P,        KC_ENT,
         KC_DEL,      KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_HOME,     KC_END,              TEMP_EN,     KC_LBRC,     KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_RBRC,     KC_BSPC,
         KC_LALT,     MO(_FN),     KC_APP,      KC_LCMD,     KC_LCTL,     KC_SPC,      KC_LSFT,     KC_ENT,              TD(TD_LALT), TD(TD_RSFT), KC_SPC,      TD(TD_RCTL), KC_LEFT,     KC_UP,       KC_DOWN,     KC_RGHT
@@ -274,6 +275,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case ARROW_THIN:
             if (record->event.pressed) SEND_STRING("->");
+            return false;
+
+        case QUOT_SWAP:
+            // _BASE: swap the shifted/unshifted output of the quote key so an
+            // unshifted tap produces " and a shifted tap produces '.
+            if (record->event.pressed) {
+                uint8_t shift = get_mods() & MOD_MASK_SHIFT;
+                if (shift) {
+                    unregister_mods(shift);
+                    tap_code(KC_QUOT);
+                    register_mods(shift);
+                } else {
+                    tap_code16(S(KC_QUOT));
+                }
+            }
             return false;
     }
     return true;
