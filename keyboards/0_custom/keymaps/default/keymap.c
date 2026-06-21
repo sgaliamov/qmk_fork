64294f956ec48c7552b5bb8505aba64e7b32bd68
s. galiamov <6alyamov@gmail.com>
2026-06-21 21:01:01 +0200

    done?

--- keyboards/0_custom/keymaps/default/keymap.c
+++ keyboards/0_custom/keymaps/default/keymap.c
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_ESC,      KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       XXXXXXX,     XXXXXXX,             XXXXXXX,     XXXXXXX,     KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      KC_PGUP,
         KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_F6,       XXXXXXX,             XXXXXXX,     KC_F7,       KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_PGDN,
-        KC_MINS,     KC_EQL,      KC_M,        KC_U,        KC_B,        KC_COMM,     KC_CAPS,     XXXXXXX,             XXXXXXX,     KC_PAUS,     KC_DOT,      KC_H,        KC_T,        KC_C,        KC_LBRC,     KC_RBRC,
-        KC_ENT,      KC_L,        KC_R,        KC_E,        KC_O,        KC_P,        KC_SLSH,     KC_BSLS,             KC_PSCR,     QUOT_SWAP,   KC_K,        KC_I,        KC_N,        KC_A,        KC_S,        KC_ENT,
-        KC_TAB,      KC_W,        KC_F,        KC_Y,        KC_D,        KC_X,        KC_HOME,     KC_END,              TD(TD_LALT), KC_SCLN,     KC_Q,        KC_G,        KC_V,        KC_J,        KC_Z,        KC_BSPC,
+        KC_MINS,     KC_EQL,      KC_C,        KC_T,        KC_H,        KC_COMM,     KC_CAPS,     XXXXXXX,             XXXXXXX,     KC_PAUS,     KC_DOT,      KC_B,        KC_U,        KC_M,        KC_LBRC,     KC_RBRC,
+        KC_ENT,      KC_S,        KC_A,        KC_N,        KC_I,        KC_K,        KC_SLSH,     KC_BSLS,             KC_PSCR,     QUOT_SWAP,   KC_P,        KC_O,        KC_E,        KC_R,        KC_L,        KC_ENT,
+        KC_TAB,      KC_Z,        KC_J,        KC_V,        KC_G,        KC_Q,        KC_HOME,     KC_END,              TD(TD_LALT), KC_SCLN,     KC_X,        KC_D,        KC_Y,        KC_F,        KC_W,        KC_BSPC,
         KC_LALT,     MO(_FN),     KC_APP,      KC_LCMD,     KC_LCTL,     KC_SPC,      KC_LSFT,     KC_DEL,              KC_INS,      TD(TD_RSFT), KC_SPC,      TD(TD_RCTL), KC_LEFT,     KC_UP,       KC_DOWN,     KC_RGHT
     ),
 
