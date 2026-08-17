60b2d4656d68cf6d18cef1073bad44d12505d7e1
s. galiamov <6alyamov@gmail.com>
2026-08-17 20:31:45 +0200

    qwerty

--- keyboards/0_custom_qwerty/keymaps/default/keymap.c
+++ keyboards/0_custom_qwerty/keymaps/default/keymap.c
@@ -17,13 +17,13 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_ESC,      KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       XXXXXXX,     XXXXXXX,             XXXXXXX,     XXXXXXX,     KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      KC_RALT,
+        KC_ESC,      KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       XXXXXXX,     XXXXXXX,             XXXXXXX,     XXXXXXX,     KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      KC_PSCR,
         KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_F6,       XXXXXXX,             XXXXXXX,     KC_F7,       KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_RBRC,
         KC_BSLS,     KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_CAPS,     XXXXXXX,             XXXXXXX,     KC_PAUS,     KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,        KC_LBRC,
-        KC_TAB,      KC_A,        KC_S,        KC_D,        KC_F,        KC_G,        KC_COMM,     KC_HOME,             KC_PSCR,     KC_DOT,      KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     QUOT_SWAP,
-        KC_DEL,      KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_MINS,     KC_END,              KC_INS,      KC_EQL,      KC_N,        KC_M,        KC_SLSH,     KC_UP,       KC_RGHT,     KC_BSPC,
-        KC_LALT,     MO(_FN),     KC_APP,      KC_LCMD,     KC_LCTL,     KC_SPC,      KC_LSFT,     KC_ENT,              KC_ENT,      KC_RSFT,     KC_SPC,      KC_RCTL,     KC_LEFT,     KC_DOWN,     KC_PGDN,     KC_PGUP
-   ),
+        KC_TAB,      KC_A,        KC_S,        KC_D,        KC_F,        KC_G,        KC_SLSH,     KC_HOME,             KC_PGUP,     KC_DOT,      KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     KC_QUOT,
+        KC_DEL,      KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_MINS,     KC_END,              KC_PGDN,     KC_EQL,      KC_N,        KC_M,        KC_COMM,     KC_UP,       KC_RGHT,     KC_BSPC,
+        KC_LALT,     KC_ENT,      KC_APP,      KC_LCMD,     KC_LCTL,     KC_SPC,      KC_LSFT,     MO(_FN),             KC_INS,      KC_RSFT,     KC_SPC,      KC_RCTL,     KC_LEFT,     KC_DOWN,     KC_ENT,      KC_RALT
+    ),
 
     [_FN] = LAYOUT(
         _______,     _______,     _______,     _______,     _______,     _______,     XXXXXXX,     XXXXXXX,             XXXXXXX,     XXXXXXX,     _______,     _______,     _______,     _______,     _______,     _______,
